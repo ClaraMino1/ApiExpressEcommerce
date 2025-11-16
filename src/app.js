ebba8fe965f0132dda943a781093b39dd561e294
Clara Miño <claraa.mino12@gmail.com>
2025-11-16 13:04:59 -0300

    incorporando Mongo

--- src/app.js
+++ src/app.js
@@ -3,40 +3,51 @@ import { engine } from "express-handlebars";
 import { Server } from "socket.io";
 import ProductManager from "./ProductManager.js";
 import viewsRouter from "./routes/views.routes.js";
-import productsRoutes from "./routes/products.router.js";
+import productsRouter from "./routes/products.router.js";
+import connectMongoDB from "./config/db.js";
+import dotenv from "dotenv";
 
-const app = express();
+//inicializar variables de entorno
+dotenv.config();
+
 const PORT = 8080;
-const productManager = new ProductManager("./src/products.json");
+const app = express();
+app.use(express.json()); //permite recibir info en formato json
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+connectMongoDB();
 
-// Handlebars
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./src/views");
+//endpoints
+app.use("/api/products",productsRouter)
+
+// const productManager = new ProductManager("./src/products.json");
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+
+// // Handlebars
+// app.engine("handlebars", engine());
+// app.set("view engine", "handlebars");
+// app.set("views", "./src/views");
 
 // Servidor HTTP
 const httpServer = app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
-// Socket.IO
-const io = new Server(httpServer);
+// // Socket.IO
+// const io = new Server(httpServer);
 
-// guardamos io en app.locals para poder usarlo en cualquier router
-app.locals.io = io;
+// // guardamos io en app.locals para poder usarlo en cualquier router
+// app.locals.io = io;
 
-// rutas
-app.use("/", viewsRouter);
-app.use("/api/products", productsRoutes);
+// // rutas
+// app.use("/", viewsRouter);
 
-// conexión socket
-io.on("connection", async (socket) => {
-  console.log("Cliente conectado:", socket.id);
+// // conexión socket
+// io.on("connection", async (socket) => {
+//   console.log("Cliente conectado:", socket.id);
 
-  const products = await productManager.getProducts();
-  socket.emit("productos", products);
-});
+//   const products = await productManager.getProducts();
+//   socket.emit("productos", products);
+// });
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,51 +1,134 @@
 import express from "express";
 import ProductManager from "../ProductManager.js";
 import uploader from "../utils/uploader.js";
+import Product from "../models/product.model.js"
 
-const productsRoutes = express.Router();
-const productManager = new ProductManager("./src/products.json");
+const productsRouter = express.Router();
 
-//concatena la ruta definida en app.js
-// Crear producto con imagen
-productsRoutes.post("/", uploader.single("file"), async (req, res) => {
+productsRouter.get("/", async (req,res)=>{
   try {
-    if (!req.file)
-      return res.status(400).json({ message: "No se ha cargado ningún archivo" });
-
-    const title = req.body.title;
-    const price = req.body.price;
-    const thumbnail = "/img/" + req.file.filename;
-
-    await productManager.addProduct({ title, price, thumbnail });
-
-    // Obtener productos actualizados
-    const updatedProducts = await productManager.getProducts();
-
-    // Emitir actualización a todos los clientes conectados
-    const io = req.app.locals.io;
-    io.emit("productos", updatedProducts);
-
-    res.redirect("/realtimeproducts");
+    const products = await Product.find();
+    res.status(200).json({status: "success", payload: products});
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({status: "error", message: "error al recuperar los productos"});
   }
 });
 
-// Eliminar producto
-productsRoutes.delete("/:id", async (req, res) => {
+productsRouter.post("/", async (req,res)=>{
   try {
-    const id = req.params.id;
-    await productManager.deleteProductById(id);
-    const updatedProducts = await productManager.getProducts();
+    const newProduct = req.body; //agarro las propiedades que se pasan por post
 
-    // Emitir actualización
-    const io = req.app.locals.io;
-    io.emit("productos", updatedProducts);
+    const product = new Product(newProduct); //creo el modelo
 
-    res.json({ message: "Producto eliminado correctamente" });
+    await product.save(); //sube a la bdd
+
+    res.status(201).json({status: "success", payload: product});
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({status: "error", message: "error al crear el producto"});
   }
 });
 
-export default productsRoutes;
+productsRouter.put("/:pid", async (req,res)=>{
+  try {
+    const pid = req.params.pid;
+    const updates = req.body; //agarro las propiedades que se pasan por post
+
+    const updatedProduct = await Product.findByIdAndUpdate(pid, updates,{new: true,runValidators:true}); //devuelve la informacion ya actualizada segun las validaciones del modelo
+
+    if(!updatedProduct) return res.status(404).json({status: "error", message: "Producto no encontrado"});
+
+    res.status(200).json({status: "success", payload: updatedProduct});
+
+  } catch (error) {
+    res.status(500).json({status: "error", message: "error al modificar el producto"});
+  }
+});
+
+productsRouter.delete("/:pid", async (req,res)=>{
+  try {
+    const pid = req.params.pid;
+    const deletedProduct = await Product.findByIdAndDelete(pid);
+
+    if(!deletedProduct) return res.status(404).json({status: "error", message: "Producto no encontrado"});
+
+    res.status(200).json({status: "success", payload: deletedProduct});
+    
+  } catch (error) {
+    res.status(500).json({status: "error", message: "error al eliminar el producto"});
+  }
+});
+
+export default productsRouter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const productManager = new ProductManager("./src/products.json");
+
+// //concatena la ruta definida en app.js
+// // Crear producto con imagen
+// productsRoutes.post("/", uploader.single("file"), async (req, res) => {
+//   try {
+//     if (!req.file)
+//       return res.status(400).json({ message: "No se ha cargado ningún archivo" });
+
+//     const title = req.body.title;
+//     const price = req.body.price;
+//     const thumbnail = "/img/" + req.file.filename;
+
+//     await productManager.addProduct({ title, price, thumbnail });
+
+//     // Obtener productos actualizados
+//     const updatedProducts = await productManager.getProducts();
+
+//     // Emitir actualización a todos los clientes conectados
+//     const io = req.app.locals.io;
+//     io.emit("productos", updatedProducts);
+
+//     res.redirect("/realtimeproducts");
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+// // Eliminar producto
+// productsRoutes.delete("/:id", async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     await productManager.deleteProductById(id);
+//     const updatedProducts = await productManager.getProducts();
+
+//     // Emitir actualización
+//     const io = req.app.locals.io;
+//     io.emit("productos", updatedProducts);
+
+//     res.json({ message: "Producto eliminado correctamente" });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+// export default productsRoutes;
